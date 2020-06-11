cf8229cc3d35cf620574074356731b45e7267c9d
changbok <changbok2453@gmail.com>
2020-06-11 10:07:12 +0900

    설정중...

--- js/function2.js
+++ js/function2.js
@@ -1,10 +1,11 @@
-var o = {};
-o.whatever = function() {};
-o.whatever();
+// var o = {};
+// o.whatever = function() {};
+// o.whatever();
 
-//함수 =메서드라 불림
-
+// //함수 =메서드라 불림
+window.onload = function(){
 function creep() {return this;}
-assert(creep() === window, "창문에서 살살기어가고있음");
+assert.ok(creep() === window, "창문에서 살살기어가고있음");
 var sneak = creep;
 assert(sneak()== window, "창문에서 살금살금 움직이고있음");
+}
