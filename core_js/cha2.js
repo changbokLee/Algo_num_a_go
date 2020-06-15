ea5bf2dbfbfa0484b29f584931988beaf108c0d1
changbokLee <changbok2453@gmail.com>
2020-06-15 20:59:20 +0900

    코어자스..

--- core_js/cha2.js
+++ core_js/cha2.js
@@ -84,3 +84,16 @@
 
 // var c = sum(1,2);
 // console.log(c);
+
+var a= 1;
+var outer = function(){
+    var  inner = function(){
+        console.log(a);
+        var a= 3;
+    };
+
+    inner();
+    console.log(a);
+};
+outer();
+console.log(a);
