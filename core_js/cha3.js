a0380addc6c7b526d40dd2dab46a8d4949739467
changbok <changbok2453@gmail.com>
2020-06-16 11:44:13 +0900

    억게인

---
+++ core_js/cha3.js
@@ -0,0 +1,15 @@
+// console.log(this);
+// console.log(window);
+// console.log(this === window); //true
+
+console.log(this);
+console.log(global);
+console.log(this === global);
+
+
+var a = 1;
+console.log(a);
+console.log(windo.a);
+console.log(this.a);
+
+//자바스크립트의 모든변수는 실은 특정객체의 프로퍼티로서 동작하기때문입니다.
